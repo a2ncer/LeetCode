6a14237547d615e76ce7aaa710bf46fcd8222320
Alexander Vishnevsky <alexander.vishnevsky@holovis.com>
2020-04-20 15:11:12 +0300

    Added new implementation for ReverseInteger

--- Leetcode/Impl/ReverseInteger.cs
+++ Leetcode/Impl/ReverseInteger.cs
@@ -1,8 +1,32 @@
+using System;
+
 namespace Leetcode.Impl.ReverseInteger
 {
     public class Solution
     {
-        public int Reverse(int x)
+        const int MAX_INT_DEVIDED = int.MaxValue / 10;
+        public int Reverse(int number)
+        {
+            int reversed = 0;
+            while (number != 0)
+            {
+                if (Math.Abs(reversed) > MAX_INT_DEVIDED)
+                {
+                    return 0;
+                }
+                int reversed_x_10 = reversed * 10;
+                int number_mod_10 = number % 10;
+                if (int.MaxValue - Math.Abs(reversed_x_10) < Math.Abs(number_mod_10))
+                {
+                    return 0;
+                }
+                reversed = reversed_x_10 + number_mod_10;
+                number = number / 10;
+            }
+            return reversed;
+        }
+
+        public int Reverse2(int x)
         {
             int reversed = 0;
             int accumulator = 0;
--- Tests/LeetCodeTests/ReverseIntegerTest.cs
+++ Tests/LeetCodeTests/ReverseIntegerTest.cs
@@ -12,11 +12,15 @@ namespace LeetCodeTests
         [InlineData(-123, -321)]
         [InlineData(120, 21)]
         [InlineData(1534236469, 0)]
+        [InlineData(-1534236469, 0)]
+        [InlineData(0, 0)]
         public void Test(int input, int expected)
         {
             int actual = solution.Reverse(input);
+            int actual2 = solution.Reverse2(input);
             
             Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual2);
         }
     }
 }
